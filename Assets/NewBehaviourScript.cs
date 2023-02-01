7049b8a99a60338a385ff40a3334022370c63538
UnknownPlayer321 <124166730+UnknownPlayer321@users.noreply.github.com>
2023-02-01 09:56:44 -0500

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,12 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Salut");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
