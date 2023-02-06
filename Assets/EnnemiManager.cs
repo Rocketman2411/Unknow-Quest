3f9a026230270afa5aa452e055ecc6f46a4c2ff1
UnknownPlayer321 <124166730+UnknownPlayer321@users.noreply.github.com>
2023-02-06 14:37:03 -0500

    ennemimanager

--- Assets/EnnemiManager.cs
+++ Assets/EnnemiManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,6 +12,7 @@ public class EnnemiManager : MonoBehaviour
     public float[] distanceEnnemis;
     public float distanceEnnemiPlusProche;
     private Vector3 positionPlayer;
+    private int[] vieEnnemis;
     private void Awake()
     {
         ennemis = new List<GameObject>();
@@ -22,7 +24,12 @@ public class EnnemiManager : MonoBehaviour
 
         _playerManagerComponent = gameObject.AddComponent<PlayerManagerComponent>();
          positionPlayer = _playerManagerComponent.transform.position;
-
+         
+         vieEnnemis = new int[ennemis.Count];
+         for (int i = 0; i < vieEnnemis.Length; i++)
+         {
+             vieEnnemis[i] = ennemis[i].gameObject.GetComponent<LifeComponent>().vie;
+         }
     }
 
     private void Update()
@@ -36,9 +43,20 @@ public class EnnemiManager : MonoBehaviour
                 Mathf.Pow(ennemis[i].transform.position.z - positionPlayer.z, 2));
         }
 
+        for (int i = 0; i < vieEnnemis.Length; i++)
+        {
+            if (vieEnnemis[0] <= 0)
+            {
+                Destroy(ennemis[i].gameObject);
+                ennemis.RemoveAt(i);
+            }
+        }
+        vieEnnemis = vieEnnemis.Where(x => x != 0).ToArray();
+
         distanceEnnemiPlusProche = Mathf.Max(distanceEnnemis);
 
     }
+    
 
     
 }
