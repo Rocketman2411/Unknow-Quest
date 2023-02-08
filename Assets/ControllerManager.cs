56080ba712995333115414cac1616d288124a8f7
Rocketman2411 <hichem_bouyahia@hotmail.com>
2023-02-08 15:23:39 -0500

    Camare suit player

--- Assets/ControllerManager.cs
+++ Assets/ControllerManager.cs
@@ -7,51 +7,48 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class ControllerManager : MonoBehaviour
 {
-    [SerializeField] public Camera cam;
+    private Vector3 _previousPosition;
     
-    private CharacterController _characterController;
+    private float _speed = 50f;
+    private float _rotateSpeed = 10f;
 
-    private float speed = 5f;
-    private float turnSpeed = 90f;
+    private float _rotY;
+    private float _rotX;
     
     // Start is called before the first frame update
     void Start()
     {
-        _characterController = GetComponent<CharacterController>();
-        
+        for (int i = 0; i < Gamepad.all.Count; i++)
+        {
+            Debug.Log(Gamepad.all[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Horizontal = Input.GetAxisRaw("Horizontal");
-        float Vertival = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(Horizontal, 0f, Vertival).normalized;
-
-        if (direction.magnitude >= 0.1f)
-        {
-            if (Gamepad.all.Count > 0)
-            {
-                if (Gamepad.all[0].leftStick.left.isPressed)
-                {
-                    _characterController.Move(direction * (speed * Time.deltaTime));
-                    //transform.Translate(Vector2.left *(speed * Time.deltaTime));
-                }
-                if (Gamepad.all[0].leftStick.right.isPressed)
-                {
-                    //transform.Translate(Vector2.right *(speed * Time.deltaTime));
-                }
-                if (Gamepad.all[0].leftStick.up.isPressed)
-                {
-                    //_characterController.Move(transform.forward * Input.GetAxis("Vertical") * (speed * Time.deltaTime));
-                }
-                if (Gamepad.all[0].rightStick.right.isPressed)
-                {
-                    //cam.transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * (turnSpeed * Time.deltaTime));
-                }
-            }
-        }
+        _rotY += Input.GetAxis("Vertical") * _rotateSpeed ;
+        _rotX += Input.GetAxis("Horizontal") * _rotateSpeed;
         
+        if (Gamepad.all.Count > 0)
+        {
+            if (Gamepad.all[0].leftStick.left.isPressed)
+            {
+                transform.Translate(Vector2.left *(_speed * Time.deltaTime));
+            }
+            if (Gamepad.all[0].leftStick.right.isPressed)
+            {
+                transform.Translate(Vector2.right *(_speed * Time.deltaTime));
+            }
+            if (Gamepad.all[0].leftStick.up.isPressed)
+            {
+                transform.localPosition += Vector3.forward * (_speed * Time.deltaTime);
+            }
+            if (Gamepad.all[0].leftStick.down.isPressed)
+            {
+                transform.localPosition += Vector3.back * (_speed * Time.deltaTime);
+            }
+            
+        }
     }
 }
