1e153cd5b52680a8d61c958b6e74ed1414194449
Rocketman2411 <hichem_bouyahia@hotmail.com>
2023-02-20 13:24:07 -0500

    On refaits les mouvement

---
+++ Assets/ScriptHichem/InputManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerControls;
+    private Vector2 movementInput;
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerInput();
+            playerControls.PlayerMovement.Movement.performed += i 
+                => movementInput = i.ReadValue<Vector2>();
+        }
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
