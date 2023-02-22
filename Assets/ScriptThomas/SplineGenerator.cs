f8cced8915fa2bcc5ad83c599fdc9d0668340ae4
UnknownPlayer321 <124166730+UnknownPlayer321@users.noreply.github.com>
2023-02-22 08:33:33 -0500

    idfkosae

--- Assets/ScriptThomas/SplineGenerator.cs
+++ Assets/ScriptThomas/SplineGenerator.cs
@@ -1,31 +1,127 @@
 using System.Collections.Generic;
 using System.Linq;
+using MathNet.Numerics;
+using MathNet.Numerics.Interpolation;
 using UnityEngine;
 using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
 using Vector3 = UnityEngine.Vector3;
+using MathNet.Spatial.Euclidean;
+using Unity.Mathematics;
 
 public class SplineGenerator : MonoBehaviour
 {
-    private List<Vector3> points;
     [SerializeField] private List<Vector3> controlPoints; 
-    public List<float> GetCoeff()
+    public List<double> GetCoeff()
     {
-        List<float> composantesPointsControle = new List<float>();
-        List<float> coeffs = new List<float>();
 
-        for (int i = 0; i < controlPoints.Count * 2; i += 2)
+        int n = controlPoints.Count;
+        Vector3[] coeffs = new Vector3[n];
+        Matrix<double> Ax = Matrix<double>.Build.DenseOfArray(new double[n-1,n-1]);
+        for (int i = 0; i < n - 1; i++)
         {
-            composantesPointsControle.Add(controlPoints[i].x);
-            composantesPointsControle.Add(controlPoints[i + 1].y);
+            Ax[i, i] = 2 * controlPoints[i + 1].x - controlPoints[i].x;
+            if (i > 0)
+            {
+                Ax[i, i - 1] = controlPoints[i + 1].x - controlPoints[i].x;
+                Ax[i - 1, i] = controlPoints[i + 1].x - controlPoints[i].x;
+            }
         }
-        var A = Matrix<float>.Build.DenseOfArray(ArrayTo2DimensionalArray(composantesPointsControle.ToArray()));
-        var b = Vector<float>.Build.Dense(new float[] { 1, -2, 0 });
-        var C = A.Solve(b);
+
+        Vector<double>[] bx = new Vector<double>[n];
+        for (int i = 0; i < n - 1; i++)
+        {
+            var P_0 = Vector<double>.Build.Dense(new double[] {controlPoints[i - 1].x,controlPoints[i - 1].y,controlPoints[i - 1].z} );
+            var P_1 = Vector<double>.Build.Dense(new double[] {controlPoints[i].x,controlPoints[i].y,controlPoints[i].z} );
+            var P_2 = Vector<double>.Build.Dense(new double[] {controlPoints[i + 1].x,controlPoints[i + 1].y,controlPoints[i + 1].z} );
+            double x_0 = controlPoints[i - 1].x;
+            double x_1 = controlPoints[i].x;
+            double x_2 = controlPoints[i + 1].x;
+
+            var delta_1 = (P_1 - P_0) / (x_1 - x_0);
+            var delta_2 = (P_2 - P_1) / (x_2 - x_1);
+
+            bx[i] = (delta_2.Subtract(delta_1));
+        }
+        Matrix<double> Ay = Matrix<double>.Build.DenseOfArray(new double[n-1,n-1]);
+        for (int i = 0; i < n - 1; i++)
+        {
+            Ay[i, i] = 2 * controlPoints[i + 1].y - controlPoints[i].y;
+            if (i > 0)
+            {
+                Ay[i, i - 1] = controlPoints[i + 1].y - controlPoints[i].y;
+                Ay[i - 1, i] = controlPoints[i + 1].y - controlPoints[i].y;
+            }
+        }
+
+        Vector<double>[] by = new Vector<double>[n];
+        for (int i = 0; i < n - 1; i++)
+        {
+            var P_0 = Vector<double>.Build.Dense(new double[] {controlPoints[i - 1].x,controlPoints[i - 1].y,controlPoints[i - 1].z} );
+            var P_1 = Vector<double>.Build.Dense(new double[] {controlPoints[i].x,controlPoints[i].y,controlPoints[i].z} );
+            var P_2 = Vector<double>.Build.Dense(new double[] {controlPoints[i + 1].x,controlPoints[i + 1].y,controlPoints[i + 1].z} );
+            double y_0 = controlPoints[i - 1].y;
+            double y_1 = controlPoints[i].y;
+            double y_2 = controlPoints[i + 1].y;
+
+            var delta_1 = (P_1 - P_0) / (y_1 - y_0);
+            var delta_2 = (P_2 - P_1) / (y_2 - y_1);
+
+            by[i] = (delta_2.Subtract(delta_1));
+        }
+        Matrix<double> Az = Matrix<double>.Build.DenseOfArray(new double[n-1,n-1]);
+        for (int i = 0; i < n - 1; i++)
+        {
+            Az[i, i] = 2 * controlPoints[i + 1].z - controlPoints[i].z;
+            if (i > 0)
+            {
+                Az[i, i - 1] = controlPoints[i + 1].z - controlPoints[i].z;
+                Az[i - 1, i] = controlPoints[i + 1].z - controlPoints[i].z;
+            }
+        }
+
+        Vector<double>[] bz = new Vector<double>[n];
+        for (int i = 0; i < n - 1; i++)
+        {
+            var P_0 = Vector<double>.Build.Dense(new double[] {controlPoints[i - 1].x,controlPoints[i - 1].y,controlPoints[i - 1].z} );
+            var P_1 = Vector<double>.Build.Dense(new double[] {controlPoints[i].x,controlPoints[i].y,controlPoints[i].z} );
+            var P_2 = Vector<double>.Build.Dense(new double[] {controlPoints[i + 1].x,controlPoints[i + 1].y,controlPoints[i + 1].z} );
+            double z_0 = controlPoints[i - 1].z;
+            double z_1 = controlPoints[i].z;
+            double z_2 = controlPoints[i + 1].z;
+
+            var delta_1 = (P_1 - P_0) / (z_1 - z_0);
+            var delta_2 = (P_2 - P_1) / (z_2 -z_1);
+
+            bz[i] = (delta_2.Subtract(delta_1));
+        }
         
-        
+        var solutionX = new Vector<double>[bx.Length];
+        for (int i = 0; i < bx.Length; i++)
+        {
+            solutionX[i] = Ax.Solve(bx[i]);
+        }
+        var solutionY = new Vector<double>[by.Length];
+        for (int i = 0; i < bz.Length; i++)
+        {
+            solutionY[i] = Ay.Solve(by[i]);
+        }
+        var solutionZ = new Vector<double>[bz.Length];
+        for (int i = 0; i < bz.Length; i++)
+        {
+            solutionZ[i] = Az.Solve(bz[i]);
+        }
+
+        Vector<double>[] coefficientsX = new Vector<double>[n];
+        coefficientsX[0] = Vector<double>.Zero;
+        for (int i = 0; i < UPPER; i++)
+        {
+            
+        }
+
         return coeffs;
     }
-
+    
     private T[,] ArrayTo2DimensionalArray<T>(T[] t)
     {
         int dimensionalsize = Mathf.RoundToInt(Mathf.Sqrt(t.Length));
@@ -44,16 +140,17 @@ public class SplineGenerator : MonoBehaviour
 
     public Vector3[] GetPointsSecondaire()
     {
-        List<float> coeffs = GetCoeff();
+        List<double> coeffs = GetCoeff();
         Vector3[] pointsSecondaires = new Vector3[coeffs.Count];
-        for (int i = 0; i < coeffs.Count; i+=8)
+        for (int i = 0; i < coeffs.Count; i+=12)
         {
             for (int j = 0; j < 8; j++)
             {
                 float t = j / 8f + i/8f;
                 pointsSecondaires[i + j] = new Vector3(
-                    coeffs[i] + coeffs[i + 1] * t + coeffs[i + 2] * t * t + coeffs[i + 3] * t * t * t,
-                    coeffs[i + 4] + coeffs[i + 5] * t + coeffs[i + 6] * t * t + coeffs[i + 7] * t * t * t, 0);
+                    (float)(coeffs[i] + coeffs[i + 1] * t + coeffs[i + 2] * t * t + coeffs[i + 3] * t * t * t),
+                    (float)(coeffs[i + 4] + coeffs[i + 5] * t + coeffs[i + 6] * t * t + coeffs[i + 7] * t * t * t), 
+                    (float)(coeffs[i + 8] + coeffs[i + 9] * t + coeffs[i + 10] * t * t + coeffs[i + 11] * t * t * t));
             }
         }
         return pointsSecondaires;
